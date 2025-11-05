ea5cb92371f6b6910cfda252b6d9dc0597ab1b17
gizag889 <gizaguri0426@outlook.jp>
2025-11-05 16:05:46 +0900

    [mod]lightingcss 設定修正

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  
  
   images: {
     domains: process.env.ALLOWED_IMAGE_HOST.split(',') // 文字列を配列に変える（区切りは「,」）
@@ -14,6 +15,11 @@ const nextConfig = {
       },
     ]
   },
+
+  experimental: {
+    optimizeCss: false // lightningcssを使用しない
+  }
+
 };
 
 module.exports = nextConfig
