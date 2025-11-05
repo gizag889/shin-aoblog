29029d992f7c7bd6e72a842043a12426a64e95e0
gizag889 <gizaguri0426@outlook.jp>
2025-11-06 02:14:40 +0900

    vercel

--- src/app/category/[slug]/page/[page]/page.tsx
+++ src/app/category/[slug]/page/[page]/page.tsx
@@ -1,38 +1,38 @@
-import AppliesTypes from "@/services/PostServices";
-import { notFound } from "next/navigation";
-import CategoryClient from "../../categoryClient";
+// import AppliesTypes from "@/services/PostServices";
+// import { notFound } from "next/navigation";
+// // import CategoryClient from "../../categoryClient";
 
 
-export default async function categoryPage({ params }: { params: { slug: string, page: string}}){
+// export default async function categoryPage({ params }: { params: { slug: string, page: string}}){
 
-    const categoryId = await AppliesTypes.getCategoryIdBySlug({ slug: params.slug });
-    if (!categoryId) {
-        notFound();
-    }
+//     const categoryId = await AppliesTypes.getCategoryIdBySlug({ slug: params.slug });
+//     if (!categoryId) {
+//         notFound();
+//     }
     
-    const page = parseInt(params.page);
-    const [staticPost] = await AppliesTypes.getList({ categoryId, page });
-    if (!staticPost || staticPost.length === 0) {
-        notFound();
-    }
+//     const page = parseInt(params.page);
+//     const [staticPost] = await AppliesTypes.getList({ categoryId, page });
+//     if (!staticPost || staticPost.length === 0) {
+//         notFound();
+//     }
 
-    return <CategoryClient categoryId={categoryId} categoryList={staticPost} currentPage={page} categorySlug={params.slug} />
-}
+//     return <CategoryClient categoryId={categoryId} categoryList={staticPost} currentPage={page} categorySlug={params.slug} />
+// }
 
-export const revalidate = 10
+// export const revalidate = 10
 
-export async function generateStaticParams() {
-    const paths = await AppliesTypes.getTotalCategory();
+// export async function generateStaticParams() {
+//     const paths = await AppliesTypes.getTotalCategory();
     
-    // ページネーションも含めた全てのパスを生成
-    return paths.map((path) => {
-        const param = path.params.param;
-        if (param.length >= 4 && param[0] === 'category' && param[2] === 'page') {
-            return { 
-                slug: param[1], 
-                page: param[3] 
-            };
-        }
-        return null;
-    }).filter(item => item !== null);
-}
+//     // ページネーションも含めた全てのパスを生成
+//     return paths.map((path) => {
+//         const param = path.params.param;
+//         if (param.length >= 4 && param[0] === 'category' && param[2] === 'page') {
+//             return { 
+//                 slug: param[1], 
+//                 page: param[3] 
+//             };
+//         }
+//         return null;
+//     }).filter(item => item !== null);
+// }
