9f6fdae93147b55250f124a0ae76f710b04c50b1
gizag889 <gizaguri0426@outlook.jp>
2025-11-06 16:57:33 +0900

    vercel

--- src/components/molecules/CategoryTags.tsx
+++ src/components/molecules/CategoryTags.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import type CategoryType from '@/types/CategoryType'
+import  CategoryType from '@/types/CategoryType'
 
 const CategoryTags = ({ category }: {
     category: CategoryType
